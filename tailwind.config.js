45918be5645eaa83c43854e86ce39f438b183758
MAbdurahman <mdbdrrhm@gmail.com>
2024-02-03 09:30:38 -0600

    Add more custom colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,27 @@ module.exports = {
          'clr-blue-02-600': 'hsl(204, 62%, 47%)',
          'clr-blue-02-700': 'hsl(204, 62%, 40%)',
          'clr-blue-02-800': 'hsl(204, 62%, 34%)',
-         'clr-blue-02-900': 'hsl(204, 62%, 28%)'
+         'clr-blue-02-900': 'hsl(204, 62%, 28%)',
+         'clr-neutral-04-000': 'hsl(184, 6%, 95%)',
+         'clr-neutral-04-100': 'hsl(184, 6%, 91%)',
+         'clr-neutral-04-200': 'hsl(184, 6%, 81%)',
+         'clr-neutral-04-300': 'hsl(184, 6%, 72%)',
+         'clr-neutral-04-400': 'hsl(184, 6%, 62%)',
+         'clr-neutral-04-500': 'hsl(184, 6%, 53%)',
+         'clr-neutral-04-600': 'hsl(184, 5%, 46%)',
+         'clr-neutral-04-700': 'hsl(184, 5%, 40%)',
+         'clr-neutral-04-800': 'hsl(184, 5%, 34%)',
+         'clr-neutral-04-900': 'hsl(184, 5%, 27%)',
+         'clr-augmented-01-000': 'hsl(210, 12% 93%)',
+         'clr-augmented-01-100': 'hsl(210, 12% 86%)',
+         'clr-augmented-01-200': 'hsl(210, 12% 71%)',
+         'clr-augmented-01-300': 'hsl(210, 12% 57%)',
+         'clr-augmented-01-400': 'hsl(210, 15% 43%)',
+         'clr-augmented-01-500': 'hsl(210, 29% 29%)',
+         'clr-augmented-01-600': 'hsl(210, 29% 25%)',
+         'clr-augmented-01-700': 'hsl(210, 29% 22%)',
+         'clr-augmented-01-800': 'hsl(210, 29% 18%)',
+         'clr-augmented-01-900': 'hsl(210, 29% 15%)'
       },
       extend: {
          fontFamily: {
